44e79b4abff23bb55c5828d77dcdc73babd00910
Tlaster <tlaster@outlook.com>
2023-10-27 13:56:36 +0900

    [Android] Remove pull to refresh
    Waiting material 3 to land officially: https://issuetracker.google.com/issues/261760718

--- app/src/main/java/dev/dimension/flare/ui/component/RefreshContainer.kt
+++ app/src/main/java/dev/dimension/flare/ui/component/RefreshContainer.kt
@@ -3,17 +3,10 @@ package dev.dimension.flare.ui.component
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.pullrefresh.PullRefreshIndicator
-import androidx.compose.material3.pullrefresh.pullRefresh
-import androidx.compose.material3.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RefreshContainer(
     refreshing: Boolean,
@@ -22,18 +15,10 @@ fun RefreshContainer(
     modifier: Modifier = Modifier,
     indicatorPadding: PaddingValues = PaddingValues(0.dp),
 ) {
-    val refreshState = rememberPullRefreshState(refreshing, onRefresh)
     Box(
         modifier =
             modifier
-                .pullRefresh(refreshState),
     ) {
         content.invoke(this)
-        PullRefreshIndicator(
-            refreshing,
-            refreshState,
-            Modifier.align(Alignment.TopCenter)
-                .padding(indicatorPadding),
-        )
     }
 }

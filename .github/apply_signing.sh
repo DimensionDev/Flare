faa43318b724e8c9102edb1635bec2237729f9b5
Tlaster <tlaster@outlook.com>
2024-01-14 12:37:00 +0900

    fix build

--- .github/apply_signing.sh
+++ .github/apply_signing.sh
@@ -1,5 +1,5 @@
 if [[ ! -z "$SIGNING_KEY" ]]; then
-    echo $GOOGLE_SERVICES | base64 -d > app/google-services.json
+    echo $GOOGLE_SERVICES > app/google-services.json
     echo $SIGNING_KEY | base64 -d > key.jks
     echo "storeFile=key.jks
     storePassword=$KEY_STORE_PASSWORD

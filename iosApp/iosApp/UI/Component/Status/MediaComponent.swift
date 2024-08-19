ea441384a981439d48115f834b4483a0b1988cb1
Tlaster <tlaster@outlook.com>
2024-08-19 20:44:19 +0900

    fix ios show image button color

--- iosApp/iosApp/UI/Component/Status/MediaComponent.swift
+++ iosApp/iosApp/UI/Component/Status/MediaComponent.swift
@@ -72,6 +72,7 @@ struct MediaComponent: View {
                         }
                     }, label: {
                         Image(systemName: "eye.slash")
+                            .foregroundColor(Color(uiColor: UIColor.systemBackground))
                     })
                     .padding()
                     .buttonStyle(.borderedProminent)

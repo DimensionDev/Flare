802f99b9aaeb77a3b5211ef9ad6f222fdd321e0e
Tlaster <tlaster@outlook.com>
2024-11-29 20:49:30 +0900

    update server list order

--- shared/src/commonMain/kotlin/dev/dimension/flare/data/network/mastodon/JoinMastodonService.kt
+++ shared/src/commonMain/kotlin/dev/dimension/flare/data/network/mastodon/JoinMastodonService.kt
@@ -1,8 +1,14 @@
 package dev.dimension.flare.data.network.mastodon
 
 import dev.dimension.flare.data.network.ktorfit
+import dev.dimension.flare.data.network.mastodon.api.InstanceResources
 import dev.dimension.flare.data.network.mastodon.api.JoinMastodonResources
+import dev.dimension.flare.data.network.mastodon.api.createInstanceResources
 import dev.dimension.flare.data.network.mastodon.api.createJoinMastodonResources
 
 internal object JoinMastodonService :
     JoinMastodonResources by ktorfit("https://api.joinmastodon.org/").createJoinMastodonResources()
+
+internal class MastodonInstanceService(
+    val baseUrl: String,
+) : InstanceResources by ktorfit(baseUrl).createInstanceResources()

a4dd00832729849562c6af79232a800c8a897806
Tlaster <tlaster@outlook.com>
2025-03-13 09:38:30 +0900

    update api usage

--- app/src/main/java/dev/dimension/flare/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/dev/dimension/flare/ui/screen/home/HomeScreen.kt
@@ -651,6 +651,7 @@ private fun getDirection(
         is RssTabItem -> RssRouteDestination
     }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun Router(
     navGraph: NavHostGraphSpec,

268be9abef5545a5acbc3eed2a011723ef8aa7b1
Tlaster <tlaster@outlook.com>
2025-03-13 16:18:03 +0900

    remove linux main from shared

--- shared/src/linuxMain/kotlin/androidx/paging/compose/LazyPagingItems.linux.kt
+++
@@ -1,7 +0,0 @@
-package androidx.paging.compose
-
-import kotlinx.coroutines.Dispatchers
-import kotlin.coroutines.CoroutineContext
-
-internal actual val mainDispatcher: CoroutineContext
-    get() = Dispatchers.Main

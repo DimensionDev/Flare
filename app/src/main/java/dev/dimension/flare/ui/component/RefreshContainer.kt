58877ff27ed3be422f6f33f668fe835665515b10
Tlaster <tlaster@outlook.com>
2025-03-21 14:11:39 +0900

    fix refresh indicator

--- app/src/main/java/dev/dimension/flare/ui/component/RefreshContainer.kt
+++ app/src/main/java/dev/dimension/flare/ui/component/RefreshContainer.kt
@@ -8,9 +8,12 @@ import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,7 +24,16 @@ fun RefreshContainer(
     modifier: Modifier = Modifier,
     indicatorPadding: PaddingValues = PaddingValues(0.dp),
 ) {
+    val scope = rememberCoroutineScope()
     val refreshState = rememberPullToRefreshState()
+    // workaround for https://issuetracker.google.com/issues/248274004
+    SideEffect {
+        if (!isRefreshing && refreshState.distanceFraction == 1f) {
+            scope.launch {
+                refreshState.animateToHidden()
+            }
+        }
+    }
     PullToRefreshBox(
         isRefreshing = isRefreshing,
         onRefresh = onRefresh,

7be7779f71c432e852f2ef44ac590e2e2fa6ae05
null <sexmm@outlook.com>
2025-04-07 16:22:45 +0800

    reset the navigation bar when leaving the profile page

--- iosApp/iosApp/UI/Page/ProfileNew/UIKitView/ProfileRefreshViewController.swift
+++ iosApp/iosApp/UI/Page/ProfileNew/UIKitView/ProfileRefreshViewController.swift
@@ -446,7 +446,13 @@ class ProfileNewRefreshViewController: UIViewController {
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        // 离开时恢复系统导航栏状态
+        
+        // 离开页面时重置状态，不然 详情页会导致没appbar 
+        //
+        isShowAppBar?.wrappedValue = true
+        isShowsegmentedBackButton?.wrappedValue = false
+        
+        // 确保系统导航栏状态正确
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
@@ -484,6 +490,15 @@ class ProfileNewRefreshViewController: UIViewController {
 
     @objc private func handleBackButtonTap() {
         os_log("[📔][ProfileRefreshViewController]点击返回按钮", log: .default, type: .debug)
+        
+        // 在返回前重置导航状态 // 离开页面时重置状态，不然 详情页会导致没appbar
+        isShowAppBar?.wrappedValue = true
+        isShowsegmentedBackButton?.wrappedValue = false
+        
+        // 确保导航栏可见
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        // 执行返回操作
         navigationController?.popViewController(animated: true)
     }
 }

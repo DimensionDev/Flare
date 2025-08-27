44ad5e50b6c563d0ab0b69eef3353d151bbc734e
Tlaster <tlaster@outlook.com>
2025-08-27 12:11:05 +0900

    fix mixed timeline only load one page

--- shared/src/commonMain/kotlin/dev/dimension/flare/data/datasource/microblog/MixedRemoteMediator.kt
+++ shared/src/commonMain/kotlin/dev/dimension/flare/data/datasource/microblog/MixedRemoteMediator.kt
@@ -4,13 +4,14 @@ import SnowflakeIdGenerator
 import androidx.paging.ExperimentalPagingApi
 import dev.dimension.flare.common.BaseTimelineRemoteMediator
 import dev.dimension.flare.data.database.cache.CacheDatabase
+import dev.dimension.flare.data.database.cache.connect
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlin.uuid.Uuid
 
 internal class MixedRemoteMediator(
-    database: CacheDatabase,
+    private val database: CacheDatabase,
     private val mediators: List<BaseTimelineRemoteMediator>,
 ) : BaseTimelineRemoteMediator(database = database) {
     override val pagingKey =
@@ -36,21 +37,23 @@ internal class MixedRemoteMediator(
                 }
                 val response =
                     currentMediators
-                        .map {
+                        .mapNotNull {
+                            getSubRequest(request, it)
+                        }.map { subRequest ->
                             async {
-                                it to
-                                    runCatching { it.timeline(pageSize, request) }
-                                        .onFailure { it.printStackTrace() }
-                                        .getOrElse {
-                                            // TODO: Handle errors for each mediator
-                                            Result(
-                                                endOfPaginationReached = true,
-                                            )
-                                        }
+                                runCatching {
+                                    subRequest.load(pageSize)
+                                }.onFailure {
+                                    it.printStackTrace()
+                                }.getOrElse {
+                                    Result(endOfPaginationReached = true)
+                                }.let {
+                                    SubResponse(subRequest.mediator, it)
+                                }
                             }
                         }.awaitAll()
 
-                val timelineResult = response.flatMap { it.second.data }
+                val timelineResult = response.flatMap { it.result.data }
 
                 val mixedTimelineResult =
                     timelineResult
@@ -68,19 +71,91 @@ internal class MixedRemoteMediator(
                             )
                         }
 
+                database.connect {
+                    response.forEach {
+                        saveSubResponse(request, it)
+                    }
+                }
+
                 currentMediators =
                     response.mapNotNull {
-                        if (it.second.endOfPaginationReached) {
+                        if (it.result.endOfPaginationReached) {
                             null
                         } else {
-                            it.first
+                            it.mediator
                         }
                     }
 
                 Result(
                     endOfPaginationReached = currentMediators.isEmpty(),
                     data = mixedTimelineResult + timelineResult,
+                    nextKey = if (currentMediators.isEmpty()) null else "mixed_next_key",
+                    previousKey = null,
                 )
             }
         }
+
+    private suspend fun getSubRequest(
+        request: Request,
+        mediator: BaseTimelineRemoteMediator,
+    ): SubRequest? =
+        when (request) {
+            is Request.Append -> {
+                database
+                    .pagingTimelineDao()
+                    .getPagingKey(mediator.pagingKey)
+                    ?.nextKey
+                    ?.let(Request::Append)
+            }
+
+            is Request.Prepend ->
+                database
+                    .pagingTimelineDao()
+                    .getPagingKey(mediator.pagingKey)
+                    ?.prevKey
+                    ?.let(Request::Prepend)
+
+            is Request.Refresh -> Request.Refresh
+        }?.let {
+            SubRequest(mediator, it)
+        }
+
+    private suspend fun saveSubResponse(
+        request: Request,
+        subResponse: SubResponse,
+    ) {
+        val (mediator, result) = subResponse
+        if (request is Request.Prepend && result.previousKey != null) {
+            database.pagingTimelineDao().updatePagingKeyPrevKey(
+                pagingKey = mediator.pagingKey,
+                prevKey = result.previousKey,
+            )
+        } else if (request is Request.Append && result.nextKey != null) {
+            database.pagingTimelineDao().updatePagingKeyNextKey(
+                pagingKey = mediator.pagingKey,
+                nextKey = result.nextKey,
+            )
+        } else if (request is Request.Refresh) {
+            database.pagingTimelineDao().deletePagingKey(mediator.pagingKey)
+            database.pagingTimelineDao().insertPagingKey(
+                dev.dimension.flare.data.database.cache.model.DbPagingKey(
+                    pagingKey = mediator.pagingKey,
+                    nextKey = result.nextKey,
+                    prevKey = result.previousKey,
+                ),
+            )
+        }
+    }
+
+    private data class SubRequest(
+        val mediator: BaseTimelineRemoteMediator,
+        val request: Request,
+    ) {
+        suspend fun load(pageSize: Int) = mediator.timeline(pageSize, request)
+    }
+
+    private data class SubResponse(
+        val mediator: BaseTimelineRemoteMediator,
+        val result: Result,
+    )
 }

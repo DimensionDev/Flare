8da5156ce55b5b14991b15b0a79b3c59602e468a
Tlaster <tlaster@outlook.com>
2025-09-05 17:07:15 +0900

    create new iosApp

--- iosApp/iosApp/UI/Page/Compose/SwitUIInAppNotification.swift
+++
@@ -1,9 +0,0 @@
-import shared
-
-class SwitUIInAppNotification: InAppNotification {
-    func onError(message _: shared.Message, throwable _: KotlinThrowable) {}
-
-    func onProgress(message _: shared.Message, progress _: Int32, total _: Int32) {}
-
-    func onSuccess(message _: shared.Message) {}
-}

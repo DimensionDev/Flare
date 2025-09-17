cf937c7cc3d099de4ffdf86abac8d6e676a00cb6
Tlaster <tlaster@outlook.com>
2025-09-17 11:47:38 +0900

    remove play integrity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -152,7 +152,6 @@ dependencies {
         implementation(platform(libs.firebase.bom))
         implementation(libs.firebase.crashlytics.ktx)
         implementation(libs.firebase.analytics.ktx)
-        implementation(libs.play.integrity)
     }
 
     testImplementation(libs.junit)

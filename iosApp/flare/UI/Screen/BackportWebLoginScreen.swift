edd854b2db46a5ac289c453b2c7cc5e7ba8dd133
Tlaster <tlaster@outlook.com>
2025-10-03 16:27:07 +0900

    fix backport webview

--- iosApp/flare/UI/Screen/BackportWebLoginScreen.swift
+++ iosApp/flare/UI/Screen/BackportWebLoginScreen.swift
@@ -15,6 +15,7 @@ struct BackportWebLoginScreen: View {
         NavigationStack {
             if viewModel.canShowWebView {
                 BackportWebView(url: URL(string: url), configuration: viewModel.configuration) { webView in
+                    webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1"
                     webView.navigationDelegate = viewModel.delegate
                 }
                 .toolbar {

92748ce4eb87b6627c517ea1a9e2f979ab37b4e4
Tlaster <tlaster@outlook.com>
2025-10-20 19:07:11 +0900

    fix login input

--- shared/src/commonMain/kotlin/dev/dimension/flare/data/network/nodeinfo/NodeInfoService.kt
+++ shared/src/commonMain/kotlin/dev/dimension/flare/data/network/nodeinfo/NodeInfoService.kt
@@ -109,17 +109,25 @@ internal data object NodeInfoService {
 
     suspend fun detectPlatformType(host: String): NodeData =
         coroutineScope {
+            val hostCleaned =
+                host
+                    .trim()
+                    .removePrefix("https://")
+                    .removePrefix("http://")
+                    .removeSuffix("/")
             val xqt = listOf(xqtOldHost, "xqt.social", xqtHost)
-            if (xqt.any { it.equals(host, ignoreCase = true) }) {
+            if (xqt.any { it.equals(hostCleaned, ignoreCase = true) }) {
                 return@coroutineScope NodeData(
+                    host = hostCleaned,
                     PlatformType.xQt,
                     PlatformType.xQt.name,
                     compatibleMode = false,
                 )
             }
             val vvo = listOf(vvoHost, vvo, vvoHostShort, "vvo.social", vvoHostLong)
-            if (vvo.any { it.equals(host, ignoreCase = true) }) {
+            if (vvo.any { it.equals(hostCleaned, ignoreCase = true) }) {
                 return@coroutineScope NodeData(
+                    host = hostCleaned,
                     PlatformType.VVo,
                     PlatformType.VVo.name,
                     compatibleMode = false,
@@ -129,10 +137,11 @@ internal data object NodeInfoService {
                 async {
                     tryRun {
                         val nodeInfo =
-                            fetchNodeInfo(host)
-                                ?: throw IllegalArgumentException("NodeInfo not found: $host")
+                            fetchNodeInfo(hostCleaned)
+                                ?: throw IllegalArgumentException("NodeInfo not found: $hostCleaned")
                         if (mastodonNodeInfoName.any { it.equals(nodeInfo, ignoreCase = true) }) {
                             NodeData(
+                                host = hostCleaned,
                                 platformType = PlatformType.Mastodon,
                                 software = nodeInfo,
                                 compatibleMode =
@@ -143,6 +152,7 @@ internal data object NodeInfoService {
                             )
                         } else if (misskeyNodeInfoName.any { it.equals(nodeInfo, ignoreCase = true) }) {
                             NodeData(
+                                host = hostCleaned,
                                 platformType = PlatformType.Misskey,
                                 software = nodeInfo,
                                 compatibleMode = !nodeInfo.equals("misskey", ignoreCase = true),
@@ -150,12 +160,13 @@ internal data object NodeInfoService {
                         } else {
                             tryRun {
                                 MisskeyService(
-                                    "https://$host/api/",
+                                    "https://$hostCleaned/api/",
                                     accessTokenFlow = null,
                                 ).meta(MetaRequest()).let {
                                     requireNotNull(it.name)
                                     // should be able to use as misskey
                                     NodeData(
+                                        host = hostCleaned,
                                         platformType = PlatformType.Misskey,
                                         software = nodeInfo,
                                         compatibleMode = true,
@@ -163,10 +174,11 @@ internal data object NodeInfoService {
                                 }
                             }.getOrElse {
                                 tryRun {
-                                    MastodonInstanceService("https://$host/").instance().let {
+                                    MastodonInstanceService("https://$hostCleaned/").instance().let {
                                         requireNotNull(it.title)
                                         // should be able to use as mastodon
                                         NodeData(
+                                            host = hostCleaned,
                                             platformType = PlatformType.Mastodon,
                                             software = nodeInfo,
                                             compatibleMode = true,
@@ -181,8 +193,9 @@ internal data object NodeInfoService {
             val bluesky =
                 async {
                     tryRun {
-                        BlueskyService("https://$host").describeServer().requireResponse()
+                        BlueskyService("https://$hostCleaned").describeServer().requireResponse()
                         NodeData(
+                            host = hostCleaned,
                             platformType = PlatformType.Bluesky,
                             software = PlatformType.Bluesky.name,
                             compatibleMode = false,
@@ -194,11 +207,12 @@ internal data object NodeInfoService {
                 nodeInfo,
                 bluesky,
             ).awaitAll().firstOrNull { it != null }
-                ?: throw IllegalArgumentException("Unsupported platform: $host")
+                ?: throw IllegalArgumentException("Unsupported platform: $hostCleaned")
         }
 }
 
 public data class NodeData(
+    val host: String,
     val platformType: PlatformType,
     val software: String,
     // not officially supported, but works fine for basic features

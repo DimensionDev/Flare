ee79f5e398c7172aca2284635e09c0525c56de03
Tlaster <tlaster@outlook.com>
2025-10-24 12:28:45 +0900

    add macos share

--- desktopApp/src/main/kotlin/dev/dimension/flare/common/DesktopShareImpl.kt
+++ desktopApp/src/main/kotlin/dev/dimension/flare/common/DesktopShareImpl.kt
@@ -1,10 +1,12 @@
 package dev.dimension.flare.common
 
+import androidx.compose.ui.platform.LocalClipboardManager
 import dev.dimension.flare.Res
 import dev.dimension.flare.common.windows.WindowsBridge
 import dev.dimension.flare.copied_to_clipboard
 import dev.dimension.flare.ui.common.DesktopShare
 import dev.dimension.flare.ui.component.ComposeInAppNotification
+import java.awt.Toolkit
 import org.apache.commons.lang3.SystemUtils
 
 internal class DesktopShareImpl(
@@ -14,6 +16,12 @@ internal class DesktopShareImpl(
     override fun shareText(text: String) {
         if (SystemUtils.IS_OS_WINDOWS) {
             windowsBridge.shareText(text)
+            inAppNotification.message(Res.string.copied_to_clipboard)
+        } else {
+            Toolkit.getDefaultToolkit().systemClipboard.setContents(
+                java.awt.datatransfer.StringSelection(text),
+                null,
+            )
             inAppNotification.message(Res.string.copied_to_clipboard)
         }
     }

722735c8bbf5569647f60c003877529baab8a55a
Rsync25 <rsync252@gmail.com>
2025-10-27 06:31:38 -0300

    FIX

--- app/src/main/java/dev/dimension/flare/App.kt
+++ app/src/main/java/dev/dimension/flare/App.kt
@@ -19,7 +19,7 @@ import dev.dimension.flare.di.composeUiModule
 import io.ktor.client.HttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-
+import nostr.keys.androidContext
 class App :
     Application(),
     SingletonImageLoader.Factory {

fd1717a40663bf17873491fd35763a57e925ca1e
Tlaster <tlaster@outlook.com>
2025-11-06 15:18:34 +0900

    fix ios AVAudioSession

--- iosApp/flare/FlareApp.swift
+++ iosApp/flare/FlareApp.swift
@@ -1,9 +1,11 @@
 import SwiftUI
 import KotlinSharedUI
+import AVFAudio
 
 @main
 struct FlareApp: App {
     init() {
+        configureAudioSessionForMixing()
         ComposeUIHelper.shared.initialize(
             inAppNotification: SwiftInAppNotification.shared,
         )
@@ -17,6 +19,20 @@ struct FlareApp: App {
                     BackportFlareRoot()
                 }
             }
+        }
+    }
+    
+    func configureAudioSessionForMixing() {
+        let session = AVAudioSession.sharedInstance()
+        do {
+            if #available(iOS 17, *) {
+                try session.setCategory(.playback, mode: .default, policy: .default, options: [.mixWithOthers])
+            } else {
+                try session.setCategory(.playback, options: [.mixWithOthers])
+            }
+            try session.setActive(true)
+        } catch {
+            print("Audio session error: \(error)")
         }
     }
 }

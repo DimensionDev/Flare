372ee0f718bbcaa813e69b4bb96d1fbafa5e1382
Tlaster <tlaster@outlook.com>
2025-12-19 13:45:27 +0900

    fix ios build

--- iosApp/flare/UI/Screen/StatusDetailScreen.swift
+++ iosApp/flare/UI/Screen/StatusDetailScreen.swift
@@ -4,8 +4,16 @@ import SwiftUI
 struct StatusDetailScreen: View {
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
     @Environment(\.openURL) private var openURL
-    @StateObject private var presenter: KotlinPresenter<TimelineState>
+    @StateObject private var presenter: KotlinPresenter<StatusContextPresenterState>
     private let statusKey: MicroBlogKey
+    private var detailStatusKey: MicroBlogKey? {
+        return switch onEnum(of: presenter.state.current) {
+        case .success(let data):
+            data.data.statusKey
+        default:
+            nil
+        }
+    }
 
     init(accountType: AccountType, statusKey: MicroBlogKey) {
         self.statusKey = statusKey
@@ -14,7 +22,7 @@ struct StatusDetailScreen: View {
 
     var body: some View {
         ZStack {
-            TimelinePagingContent(data: presenter.state.listState, detailStatusKey: statusKey, key: presenter.key)
+            TimelinePagingContent(data: presenter.state.listState, detailStatusKey: detailStatusKey, key: presenter.key)
                 .frame(maxWidth: horizontalSizeClass == .compact ? .infinity : 600, alignment: .center)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)

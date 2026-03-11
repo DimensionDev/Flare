a44010dd2aef4960f4533e4973b1bbe188f37576
Tlaster <tlaster@outlook.com>
2026-03-11 20:34:59 +0900

    fix test

--- shared/src/appleTest/kotlin/dev/dimension/flare/DatabaseHelper.apple.kt
+++ shared/src/appleTest/kotlin/dev/dimension/flare/DatabaseHelper.apple.kt
@@ -3,16 +3,14 @@ package dev.dimension.flare
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import dev.dimension.flare.data.database.app.AppDatabase
-import dev.dimension.flare.data.database.app.AppDatabaseConstructor
 import dev.dimension.flare.data.database.cache.CacheDatabase
-import dev.dimension.flare.data.database.cache.CacheDatabaseConstructor
 import kotlin.reflect.KClass
 
 @Suppress("UNCHECKED_CAST")
 internal actual fun <T : RoomDatabase> Room.memoryDatabaseBuilder(databaseClass: KClass<T>): RoomDatabase.Builder<T> =
     when (databaseClass) {
-        AppDatabase::class -> Room.inMemoryDatabaseBuilder(factory = AppDatabaseConstructor::initialize)
-        CacheDatabase::class -> Room.inMemoryDatabaseBuilder(factory = CacheDatabaseConstructor::initialize)
+        AppDatabase::class -> Room.inMemoryDatabaseBuilder<AppDatabase>()
+        CacheDatabase::class -> Room.inMemoryDatabaseBuilder<CacheDatabase>()
         else -> error("Unsupported test database: ${databaseClass.qualifiedName}")
     } as RoomDatabase.Builder<T>
 

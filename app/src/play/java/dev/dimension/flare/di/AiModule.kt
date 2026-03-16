a6f85ee70ce022c4dc5cc7cf09a97398445e7c57
Tlaster <tlaster@outlook.com>
2026-03-16 15:02:32 +0900

    fix build

--- app/src/play/java/dev/dimension/flare/di/AiModule.kt
+++ app/src/play/java/dev/dimension/flare/di/AiModule.kt
@@ -4,6 +4,7 @@ import dev.dimension.flare.common.AndroidOnDeviceAI
 import dev.dimension.flare.common.OnDeviceAI
 import org.koin.dsl.module
 
-val aiModule = module {
-    single<OnDeviceAI> { AndroidOnDeviceAI(get()) }
-}
+val aiModule =
+    module {
+        single<OnDeviceAI> { AndroidOnDeviceAI(get()) }
+    }

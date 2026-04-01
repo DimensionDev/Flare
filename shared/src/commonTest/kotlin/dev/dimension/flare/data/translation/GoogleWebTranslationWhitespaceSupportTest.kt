101bbf48ea874ce8117d3af8715f7ba81a1d35aa
Tlaster <tlaster@outlook.com>
2026-04-02 02:00:30 +0900

    fix google cloud translate line break

--- shared/src/commonTest/kotlin/dev/dimension/flare/data/translation/GoogleWebTranslationWhitespaceSupportTest.kt
+++ shared/src/commonTest/kotlin/dev/dimension/flare/data/translation/GoogleWebTranslationWhitespaceSupportTest.kt
@@ -33,4 +33,26 @@ class GoogleWebTranslationWhitespaceSupportTest {
             GoogleWebTranslationWhitespaceSupport.trimBoundaryWhitespace("  Hello  world  "),
         )
     }
+
+    @Test
+    fun normalizeExpandedInternalNewlines_collapsesUnexpectedBlankLines() {
+        assertEquals(
+            "甲段\n乙标记\n丙值\n丁标记\n戊值\n己尾部",
+            GoogleWebTranslationWhitespaceSupport.normalizeExpandedInternalNewlines(
+                sourceText = "line-a\nlabel-b\nvalue-c\nlabel-d\nvalue-e\nline-f",
+                translatedText = "甲段\n\n乙标记\n\n丙值\n\n丁标记\n\n戊值\n\n己尾部",
+            ),
+        )
+    }
+
+    @Test
+    fun normalizeExpandedInternalNewlines_keepsExistingParagraphBreaks() {
+        assertEquals(
+            "第一段\n\n第二段",
+            GoogleWebTranslationWhitespaceSupport.normalizeExpandedInternalNewlines(
+                sourceText = "First paragraph\n\nSecond paragraph",
+                translatedText = "第一段\n\n第二段",
+            ),
+        )
+    }
 }

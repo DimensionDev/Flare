8e414459ee16593d1bfd999f2a08fc48a0dbb21c
Tlaster <tlaster@outlook.com>
2026-04-07 14:14:54 +0900

    enhance ios status action view performance

--- iosApp/flare/UI/Component/Status/TimelineUserView.swift
+++ iosApp/flare/UI/Component/Status/TimelineUserView.swift
@@ -3,6 +3,8 @@ import KotlinSharedUI
 
 struct TimelineUserView: View {
     @Environment(\.openURL) private var openURL
+    @Environment(\.appearanceSettings.showNumbers) private var showNumbers
+    @ScaledMetric(relativeTo: .footnote) var fontSize = 13
     let data: UiTimelineV2.User
     var body: some View {
         VStack {
@@ -13,7 +15,7 @@ struct TimelineUserView: View {
             if !data.button.isEmpty {
                 HStack {
                     ForEach(Array(data.button.enumerated()), id: \.offset) { _, button in
-                        StatusActionItemView(data: button, useText: true, isFixedWidth: false)
+                        StatusActionItemView(data: button, useText: true, isFixedWidth: false, fontSize: fontSize, showNumbers: showNumbers, openURL: openURL)
                     }
                 }
             }

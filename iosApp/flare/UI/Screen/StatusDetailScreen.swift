5f82def73bd2b754f6893b5149d2d0253c8c2df4
Tlaster <tlaster@outlook.com>
2026-04-10 13:51:54 +0900

    fix ios status detail

--- iosApp/flare/UI/Screen/StatusDetailScreen.swift
+++ iosApp/flare/UI/Screen/StatusDetailScreen.swift
@@ -6,14 +6,6 @@ struct StatusDetailScreen: View {
     @Environment(\.openURL) private var openURL
     @StateObject private var presenter: KotlinPresenter<StatusContextPresenterState>
     private let statusKey: MicroBlogKey
-    private var detailStatusKey: MicroBlogKey? {
-        return switch onEnum(of: presenter.state.current) {
-        case .success(let data):
-            (data.data as? UiTimelineV2.Post)?.statusKey
-        default:
-            nil
-        }
-    }
 
     init(accountType: AccountType, statusKey: MicroBlogKey) {
         self.statusKey = statusKey
@@ -22,7 +14,7 @@ struct StatusDetailScreen: View {
 
     var body: some View {
         ZStack {
-            TimelinePagingContent(data: presenter.state.listState, detailStatusKey: detailStatusKey, key: presenter.key)
+            TimelinePagingContent(data: presenter.state.listState, detailStatusKey: statusKey, key: presenter.key)
                 .frame(maxWidth: horizontalSizeClass == .compact ? .infinity : 600, alignment: .center)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)

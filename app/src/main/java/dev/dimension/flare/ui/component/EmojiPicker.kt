25e2c19ffb1a7d9866e9a8661ca5a028ad04613d
Tlaster <tlaster@outlook.com>
2026-04-15 16:19:44 +0900

    add expander for emoji

--- app/src/main/java/dev/dimension/flare/ui/component/EmojiPicker.kt
+++ app/src/main/java/dev/dimension/flare/ui/component/EmojiPicker.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
@@ -15,20 +16,27 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SegmentedListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.CaretDown
+import compose.icons.fontawesomeicons.solid.CaretUp
 import dev.dimension.flare.R
 import dev.dimension.flare.model.AccountType
 import dev.dimension.flare.ui.model.UiEmoji
 import dev.dimension.flare.ui.model.onSuccess
 import dev.dimension.flare.ui.presenter.compose.EmojiHistoryPresenter
 import dev.dimension.flare.ui.presenter.invoke
+import dev.dimension.flare.ui.theme.segmentedShapes2
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.ImmutableMap
 import kotlinx.collections.immutable.toImmutableList
@@ -49,6 +57,7 @@ internal fun EmojiPicker(
             data = data,
         )
     }
+    val expandedCategory = remember { mutableStateListOf<String>() }
     val searchTextState = rememberTextFieldState()
     val actualData =
         remember(data, searchTextState.text) {
@@ -74,8 +83,8 @@ internal fun EmojiPicker(
         LazyVerticalGrid(
             columns = GridCells.Adaptive(36.dp),
             state = gridListState,
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(2.dp),
+            verticalArrangement = Arrangement.spacedBy(2.dp),
         ) {
             state.history.onSuccess { history ->
                 if (searchTextState.text.isEmpty() && history.isNotEmpty()) {
@@ -120,41 +129,67 @@ internal fun EmojiPicker(
                 }
             }
 
-            actualData.forEach { (category, emojis) ->
+            actualData.onEachIndexed { index, (category, emojis) ->
                 if (emojis.any()) {
-                    if (category.isNotEmpty() && category.isNotBlank()) {
+                    if (actualData.size > 1) {
                         item(
                             span = {
                                 GridItemSpan(maxLineSpan)
                             },
                         ) {
-                            ListItem(
+                            SegmentedListItem(
+                                selected = expandedCategory.contains(category),
+                                onClick = {
+                                    if (expandedCategory.contains(category)) {
+                                        expandedCategory.remove(category)
+                                    } else {
+                                        expandedCategory.add(category)
+                                    }
+                                },
+                                shapes =
+                                    ListItemDefaults.segmentedShapes2(
+                                        index = index,
+                                        count = data.size,
+                                    ),
                                 colors =
                                     ListItemDefaults.colors(
                                         containerColor = Color.Transparent,
                                     ),
-                                headlineContent = {
+                                content = {
                                     Text(category)
+                                },
+                                trailingContent = {
+                                    FAIcon(
+                                        if (expandedCategory.contains(category)) {
+                                            FontAwesomeIcons.Solid.CaretUp
+                                        } else {
+                                            FontAwesomeIcons.Solid.CaretDown
+                                        },
+                                        contentDescription = null,
+                                    )
                                 },
                             )
                         }
                     }
 
-                    items(
-                        emojis.size,
-                    ) { index ->
-                        val emoji = emojis[index]
-                        NetworkImage(
-                            model = emoji.url,
-                            contentDescription = emoji.shortcode,
-                            modifier =
-                                Modifier
-                                    .size(36.dp)
-                                    .clickable {
-                                        onEmojiSelected(emoji)
-                                        state.addHistory(emoji)
-                                    },
-                        )
+                    if (expandedCategory.contains(category) || actualData.size == 1) {
+                        items(
+                            emojis.size,
+                        ) { index ->
+                            val emoji = emojis[index]
+                            NetworkImage(
+                                model = emoji.url,
+                                contentDescription = emoji.shortcode,
+                                modifier =
+                                    Modifier
+                                        .padding(2.dp)
+                                        .size(36.dp)
+                                        .clickable {
+                                            onEmojiSelected(emoji)
+                                            state.addHistory(emoji)
+                                        },
+                            )
+                        }
                     }
                 }
             }
--- desktopApp/src/main/kotlin/dev/dimension/flare/ui/component/EmojiPicker.kt
+++ desktopApp/src/main/kotlin/dev/dimension/flare/ui/component/EmojiPicker.kt
@@ -1,9 +1,11 @@
 package dev.dimension.flare.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
@@ -11,19 +13,26 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.CaretDown
+import compose.icons.fontawesomeicons.solid.CaretUp
 import dev.dimension.flare.Res
 import dev.dimension.flare.emoji_picker_recent
 import dev.dimension.flare.emoji_picker_search
 import dev.dimension.flare.model.AccountType
+import dev.dimension.flare.ui.component.status.ListComponent
 import dev.dimension.flare.ui.model.UiEmoji
 import dev.dimension.flare.ui.model.onSuccess
 import dev.dimension.flare.ui.presenter.compose.EmojiHistoryPresenter
 import dev.dimension.flare.ui.presenter.invoke
+import io.github.composefluent.FluentTheme
 import io.github.composefluent.component.ListHeader
 import io.github.composefluent.component.Text
 import io.github.composefluent.component.TextField
@@ -48,6 +57,7 @@ internal fun EmojiPicker(
             data = data,
         )
     }
+    val expandedCategory = remember { mutableStateListOf<String>() }
     var text by remember { mutableStateOf("") }
     val actualData =
         remember(data, text) {
@@ -60,89 +70,117 @@ internal fun EmojiPicker(
             }
         }
     val gridListState = rememberLazyGridState()
-    Column(
+    FlareScrollBar(
         modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+        state = gridListState,
     ) {
-        TextField(
-            value = text,
-            onValueChange = { text = it },
-            placeholder = { Text(stringResource(Res.string.emoji_picker_search)) },
-            maxLines = 1,
-            modifier = Modifier.fillMaxWidth(),
-        )
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(32.dp),
-            state = gridListState,
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        Column(
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            state.history.onSuccess { history ->
-                if (text.isEmpty() && history.isNotEmpty()) {
-                    item(
-                        span = {
-                            GridItemSpan(maxLineSpan)
-                        },
-                    ) {
-                        ListHeader(
-                            content = {
-                                Text(stringResource(Res.string.emoji_picker_recent))
-                            },
-                        )
-                    }
-                    items(
-                        history.size,
-                    ) { index ->
-                        val emoji = history[index]
-                        NetworkImage(
-                            model = emoji.url,
-                            contentDescription = emoji.shortcode,
-                            modifier =
-                                Modifier
-                                    .size(32.dp)
-                                    .clickable {
-                                        onEmojiSelected(emoji)
-                                        state.addHistory(emoji)
-                                    },
-                        )
-                    }
-                }
-//                item(
-//                    span = {
-//                        GridItemSpan(maxLineSpan)
-//                    },
-//                ) {
-//                    HorizontalDivider()
-//                }
-            }
-
-            actualData.forEach { (category, emojis) ->
-                if (emojis.any()) {
-                    if (category.isNotEmpty() && category.isNotBlank()) {
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                placeholder = { Text(stringResource(Res.string.emoji_picker_search)) },
+                maxLines = 1,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(32.dp),
+                state = gridListState,
+                horizontalArrangement = Arrangement.spacedBy(2.dp),
+                verticalArrangement = Arrangement.spacedBy(2.dp),
+            ) {
+                state.history.onSuccess { history ->
+                    if (text.isEmpty() && history.isNotEmpty()) {
                         item(
                             span = {
                                 GridItemSpan(maxLineSpan)
                             },
                         ) {
-                            Text(category)
+                            ListHeader(
+                                content = {
+                                    Text(stringResource(Res.string.emoji_picker_recent))
+                                },
+                            )
+                        }
+                        items(
+                            history.size,
+                        ) { index ->
+                            val emoji = history[index]
+                            NetworkImage(
+                                model = emoji.url,
+                                contentDescription = emoji.shortcode,
+                                modifier =
+                                    Modifier
+                                        .size(32.dp)
+                                        .clickable {
+                                            onEmojiSelected(emoji)
+                                            state.addHistory(emoji)
+                                        },
+                            )
                         }
                     }
+                }
 
-                    items(
-                        emojis.size,
-                    ) { index ->
-                        val emoji = emojis[index]
-                        NetworkImage(
-                            model = emoji.url,
-                            contentDescription = emoji.shortcode,
-                            modifier =
-                                Modifier
-                                    .size(32.dp)
-                                    .clickable {
-                                        onEmojiSelected(emoji)
-                                        state.addHistory(emoji)
+                actualData.forEach { (category, emojis) ->
+                    if (emojis.any()) {
+                        if (actualData.size > 1) {
+                            item(
+                                span = {
+                                    GridItemSpan(maxLineSpan)
+                                },
+                            ) {
+                                ListComponent(
+                                    headlineContent = {
+                                        Text(category)
                                     },
-                        )
+                                    trailingContent = {
+                                        FAIcon(
+                                            if (expandedCategory.contains(category)) {
+                                                FontAwesomeIcons.Solid.CaretUp
+                                            } else {
+                                                FontAwesomeIcons.Solid.CaretDown
+                                            },
+                                            contentDescription = null,
+                                        )
+                                    },
+                                    modifier =
+                                        Modifier
+                                            .background(
+                                                FluentTheme.colors.control.default,
+                                            ).padding(
+                                                horizontal = 16.dp,
+                                                vertical = 8.dp,
+                                            ).clickable {
+                                                if (expandedCategory.contains(category)) {
+                                                    expandedCategory.remove(category)
+                                                } else {
+                                                    expandedCategory.add(category)
+                                                }
+                                            },
+                                )
+                            }
+                        }
+
+                        if (expandedCategory.contains(category) || actualData.size == 1) {
+                            items(
+                                emojis.size,
+                            ) { index ->
+                                val emoji = emojis[index]
+                                NetworkImage(
+                                    model = emoji.url,
+                                    contentDescription = emoji.shortcode,
+                                    modifier =
+                                        Modifier
+                                            .padding(2.dp)
+                                            .size(32.dp)
+                                            .clickable {
+                                                onEmojiSelected(emoji)
+                                                state.addHistory(emoji)
+                                            },
+                                )
+                            }
+                        }
                     }
                 }
             }

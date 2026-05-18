5372d3f180bd5818900c51163cd780550f7d65fd
Tlaster <tlaster@outlook.com>
2026-05-19 01:49:10 +0900

    Move settings export models to data repository

--- data/repository/src/commonMain/kotlin/dev/dimension/flare/data/model/SettingsExport.kt
+++ data/repository/src/commonMain/kotlin/dev/dimension/flare/data/model/SettingsExport.kt
@@ -2,18 +2,19 @@ package dev.dimension.flare.data.model
 
 import dev.dimension.flare.data.datastore.model.AppSettings
 import dev.dimension.flare.data.model.appearance.AppearanceBag
+import dev.dimension.flare.data.model.tab.TabSettingsV2
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class LegacySettingsExport(
+public data class SettingsExport(
     val appearanceBag: AppearanceBag,
     val appSettings: AppSettings,
-    val tabSettings: TabSettings,
+    val tabSettingsV2: TabSettingsV2,
 )
 
 @Serializable
-internal data class LegacyAppearanceSettingsAndTabsExport(
+public data class LegacyAppearanceSettingsExport(
     val appearanceSettings: AppearanceSettings,
     val appSettings: AppSettings,
-    val tabSettings: TabSettings,
+    val tabSettingsV2: TabSettingsV2,
 )

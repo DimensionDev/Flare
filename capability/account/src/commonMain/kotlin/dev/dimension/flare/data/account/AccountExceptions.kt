2d79f09ef469ba7a9bc29aed03d644d29161fa96
Tlaster <tlaster@outlook.com>
2026-05-19 17:27:53 +0900

    Remove account exception aliases

--- capability/account/src/commonMain/kotlin/dev/dimension/flare/data/account/AccountExceptions.kt
+++ capability/account/src/commonMain/kotlin/dev/dimension/flare/data/account/AccountExceptions.kt
@@ -1,7 +1,3 @@
 package dev.dimension.flare.data.account
 
 public data object NoActiveAccountException : Exception("No active account.")
-
-public typealias LoginExpiredException = dev.dimension.flare.model.LoginExpiredException
-
-public typealias RequireReLoginException = dev.dimension.flare.model.RequireReLoginException

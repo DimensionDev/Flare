6e905dac90ff4be42b9000a7d3abd4dff795a386
Tlaster <tlaster@outlook.com>
2026-05-19 12:01:53 +0900

    Share bundled SQLite driver for non-web

--- data/database/src/appleMain/kotlin/dev/dimension/flare/data/database/DatabaseDriverProvider.apple.kt
+++
@@ -1,6 +0,0 @@
-package dev.dimension.flare.data.database
-
-import androidx.sqlite.SQLiteDriver
-import androidx.sqlite.driver.bundled.BundledSQLiteDriver
-
-public actual fun createDatabaseDriver(): SQLiteDriver = BundledSQLiteDriver()

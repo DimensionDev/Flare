4a1c9a83f1add694b0f1536beca27a775961b32d
Tlaster <tlaster@outlook.com>
2026-05-20 20:36:33 +0900

    chore: scaffold modular project layout

---
+++ foundation/database/build.gradle.kts
@@ -0,0 +1,20 @@
+import dev.dimension.flare.buildlogic.FlarePlatform
+import dev.dimension.flare.buildlogic.flare
+
+plugins {
+    id("dev.dimension.flare.multiplatform-library")
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.multiplatform)
+}
+
+kotlin {
+    flare {
+        namespace = "dev.dimension.flare.foundation.database"
+        platforms(
+            FlarePlatform.ANDROID,
+            FlarePlatform.JVM,
+            FlarePlatform.IOS,
+            FlarePlatform.WEB,
+        )
+    }
+}

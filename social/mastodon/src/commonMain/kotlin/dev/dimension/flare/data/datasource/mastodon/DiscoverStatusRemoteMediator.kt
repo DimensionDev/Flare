d4ef99bd7bd78060979ce49826f87149b0c5d48a
Tlaster <tlaster@outlook.com>
2026-08-12 13:23:29 +0800

    fix mastodon discover

--- social/mastodon/src/commonMain/kotlin/dev/dimension/flare/data/datasource/mastodon/DiscoverStatusRemoteMediator.kt
+++ social/mastodon/src/commonMain/kotlin/dev/dimension/flare/data/datasource/mastodon/DiscoverStatusRemoteMediator.kt
@@ -4,14 +4,14 @@ import androidx.paging.ExperimentalPagingApi
 import dev.dimension.flare.data.datasource.microblog.paging.CacheableRemoteLoader
 import dev.dimension.flare.data.datasource.microblog.paging.PagingRequest
 import dev.dimension.flare.data.datasource.microblog.paging.PagingResult
-import dev.dimension.flare.data.network.mastodon.MastodonService
+import dev.dimension.flare.data.network.mastodon.api.TrendsResources
 import dev.dimension.flare.model.MicroBlogKey
 import dev.dimension.flare.ui.model.UiTimelineV2
 import dev.dimension.flare.ui.model.mapper.render
 
 @OptIn(ExperimentalPagingApi::class)
 internal class DiscoverStatusRemoteMediator(
-    private val service: MastodonService,
+    private val service: TrendsResources,
     private val accountKey: MicroBlogKey,
 ) : CacheableRemoteLoader<UiTimelineV2> {
     override val pagingKey: String = "discover_status_$accountKey"
@@ -20,22 +20,28 @@ internal class DiscoverStatusRemoteMediator(
         pageSize: Int,
         request: PagingRequest,
     ): PagingResult<UiTimelineV2> {
-        val response =
+        val offset =
             when (request) {
                 PagingRequest.Refresh -> {
-                    service.trendsStatuses()
+                    0
                 }
 
-                else -> {
+                is PagingRequest.Append -> {
+                    request.nextKey.toIntOrNull() ?: 0
+                }
+
+                is PagingRequest.Prepend -> {
                     return PagingResult(
                         endOfPaginationReached = true,
                     )
                 }
             }
 
+        val response = service.trendsStatuses(limit = pageSize, offset = offset)
         return PagingResult(
-            endOfPaginationReached = true,
+            endOfPaginationReached = response.size < pageSize,
             data = response.render(accountKey),
+            nextKey = (offset + response.size).toString(),
         )
     }
 }
